134d02d91fcd71d74c4eec600398124532280104
Taka Okunishi <okunishitaka.com@gmail.com>
2016-08-01 22:52:57 +0900

    Refactoing

--- test/awatch_test.js
+++ test/awatch_test.js
@@ -6,6 +6,8 @@
 
 const awatch = require('../lib/awatch.js')
 const assert = require('assert')
+const path = require('path')
+const fs = require('fs')
 const co = require('co')
 const asleep = require('asleep')
 const writeout = require('writeout')
@@ -26,12 +28,19 @@ describe('awatch', function () {
     yield writeout(filename, 'hoge', { mkdirp: true, force: true })
     let changed = {}
     let close = yield awatch(`${__dirname}/../tmp/*`, (event, filename) => {
-      changed[ `${event}:${filename}` ] = { event, filename }
-    })
-    yield writeout(filename, 'hoge2', { mkdirp: true, force: true })
-
-    yield asleep(300)
-    assert.ok(changed, 'rename:bar.txt')
+      changed[ filename ] = (changed[ filename ] || 0) + 1
+    }, { interval: 10 })
+    yield writeout(filename, 'hoge2')
+    yield asleep(100)
+    assert.equal(changed[ path.basename(filename) ], 1)
+    yield writeout(filename, 'hoge3')
+    yield asleep(100)
+    assert.equal(changed[ path.basename(filename) ], 2)
+    yield asleep(100)
+    fs.renameSync(filename, `${filename}.bk`)
+    yield writeout(filename, 'hoge4')
+    yield asleep(100)
+    assert.equal(changed[ path.basename(filename) ], 3)
     yield close()
   }))
 })
